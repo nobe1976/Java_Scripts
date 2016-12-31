b62cb350425279887066c2cce8db24773291efe5
no1uknow <nobe1976@users.noreply.github.com>
2016-12-30 22:56:58 -0500

    Proper Open Java for Building Android N

--- Java8_installer.sh
+++ Java8_installer.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 echo -e $YELLOW"Purge IcedTea..."$ENDCOLOR
-sudo apt-get --purge autoremove icedtea*
+sudo apt-get --purge autoremove icedtea* -y
  
 echo -e $YELLOW"Removing old config files..."$ENDCOLOR
-sudo apt-get --purge autoremove openjdk*
+sudo apt-get --purge autoremove openjdk* -y
 
-sudo apt-get --purge autoremove java-common libservlet2.5-java libxerces2-java libxml-commons-external-java libxml-commons-resolver1.1-java tzdata-java
+sudo apt-get --purge autoremove java-common libservlet2.5-java libxerces2-java libxml-commons-external-java libxml-commons-resolver1.1-java tzdata-java -y
  
 echo -e $YELLOW"Installing Java 8..."$ENDCOLOR
-sudo add-apt-repository ppa:webupd8team/java
-
+sudo apt-add-repository ppa:openjdk-r/ppa -y
 sudo apt-get update
-
-sudo apt-get install oracle-java8-installer
+sudo apt-get install openjdk-8-jdk -y
+sudo apt-get install openjdk-8-jre -y
 
 echo -e $YELLOW"Script Finished!"$ENDCOLOR
